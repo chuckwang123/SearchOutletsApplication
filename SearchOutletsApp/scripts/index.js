99ce64bca78b7b3cf48bb102e40403d1b56abdcd
chuckwang123 <wangjohn419@gmail.com>
2016-08-09 21:43:53 -0500

    Add back and clean up front End

--- SearchOutletsApp/scripts/index.js
+++ SearchOutletsApp/scripts/index.js
@@ -11,7 +11,7 @@ function GenerateIndexPage() {
     LoadTable();
 }
 function GenerateUserTemplate() {
-    var userTemplate = '<div class="table-content"><a href="/" class="nav-link"><h3 class="page-header">User Dashboard</h3></a>\r\n    <table id="table" data-pagination="true" data-show-refresh="true" data-sort-name="FirstName" data-sort-order="asc" data-search="true" data-toolbar=".toolbar">\r\n        <thead>\r\n            <tr id="header">\r\n                <th data-field="Name" data-width="11%" data-sortable="true">Name <i class="fa fa-level-down" aria-hidden="true"></i></i>\r\n                </th>\r\n                <th data-field="OutletName" data-align="center" data-width="16%" data-sortable="true" class="jira-key-style">Outlet Name <i class="fa fa-level-down" aria-hidden="true"></i></i>\r\n                </th>\r\n                <th data-field="Title" data-align="center" data-width="14%" data-sortable="true" class="jira-key-style">Title <i class="fa fa-level-down" aria-hidden="true"></i></i>\r\n                </th>\r\n                <th data-field="Profile" data-align="center" data-width="14%" data-sortable="true" class="latest-email-style">Profile <i class="fa fa-level-down" aria-hidden="true"></i></i>\r\n                </th>\r\n            </tr>\r\n        </thead>\r\n    </table>\r\n</div>';
+    var userTemplate = '<div class="table-content"><a class="nav-link"><h3 class="page-header">User Dashboard</h3></a>\r\n    <table id="table" data-pagination="true" data-show-refresh="true" data-sort-name="Name" data-sort-order="asc" data-search="true" data-toolbar=".toolbar">\r\n        <thead>\r\n            <tr id="header">\r\n                <th data-field="Name" data-width="11%" data-sortable="true">Name <i class="fa fa-level-down" aria-hidden="true"></i></i>\r\n                </th>\r\n                <th data-field="OutletName" data-align="center" data-width="16%" data-sortable="true" >Outlet Name <i class="fa fa-level-down" aria-hidden="true"></i>\r\n                </th>\r\n                <th data-field="Title" data-align="center" data-width="14%" data-sortable="true" >Title <i class="fa fa-level-down" aria-hidden="true"></i>\r\n                </th>\r\n                <th data-field="Profile" data-align="center" data-width="14%" data-sortable="true" >Profile <i class="fa fa-level-down" aria-hidden="true"></i>\r\n                </th>\r\n            </tr>\r\n        </thead>\r\n    </table>\r\n</div>';
     Mustache.parse(userTemplate);   // optional, speeds up future uses
     var rendered = Mustache.render(userTemplate);
     $('#content').html(rendered);
@@ -38,5 +38,28 @@ function GetData(apiUrl) {
         crossDomain: true,
         contentType: "application/json"
     });
+}
+
+function NotifyErrorAndThrow(message) {
+    message = typeof message !== 'undefined' ? message : 'Failed to connect to the server!'; // default parameter
+    var warningTemplate = '<div class="text-center"><i class="fa fa-exclamation-triangle fail-load" aria-hidden="true"></i></div>';
+    Mustache.parse(warningTemplate);   // optional, speeds up future uses
+    var rendered = Mustache.render(warningTemplate);
+    $('#content').html(rendered);
+    $.notify({
+        icon: 'fa fa-exclamation-triangle',
+        message: message
+    },
+	{
+	    offset: {
+	        y: 80
+	    },
+	    allow_dismiss: false,
+	    delay: 0,
+	    type: 'danger',
+	    placement: {
+	        align: 'center'
+	    }
+	});
 }
 
